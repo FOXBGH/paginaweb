5b30df37d6b6646560f6255b97121f12ac0ed3a6
FOXBGH <115656401+FOXBGH@users.noreply.github.com>
2022-10-12 18:32:42 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+
+ScrollReveal().reveal('.showCase');
+ScrollReveal().reveal('.news-cards', {delay:500});
+ScrollReveal().reveal('.cards-banners-one', {delay:500});
+ScrollReveal().reveal('.cards-banners-two', {delay:500});
